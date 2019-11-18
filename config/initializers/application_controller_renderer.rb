57f520e87c62a8e74d265182ccb6fdb76ae41dee
kipe <tkrs23280810@gmail.com>
2019-11-18 14:55:13 +0900

    [Finish]3-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '214d4f49872c8b790a94fd6f6e5b01c8ef4bcc4b647b730a337130e02030c5810f446329f7e8fde678e7247f9d2c4c05d1ee5e3d9f2d14b653706f4d9b4ad4db'
